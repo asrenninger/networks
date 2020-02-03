3052fac17ba4a04902fd01757b22b28e4a5e7091
andrew <arenn@design.upenn.edu>
2020-02-02 21:39:58 -0500

    Commit from RStudio

---
+++ R/rebenchmark.R
@@ -0,0 +1,117 @@
+library(tigris)
+library(sf)
+
+options(tigris_use_cache = TRUE)
+
+##
+
+roads <- roads("PA", "Philadelphia", class = 'sf')
+tracts <- tracts("PA", "Philadelphia", class = 'sf')
+
+water <- 
+  area_water("PA", "Philadelphia", class = 'sf') %>%
+  st_union() %>%
+  st_combine()
+
+##
+
+library(tidyverse)
+
+##
+
+background <-
+  tracts %>%
+  mutate(dissolve = 1) %>%
+  group_by(dissolve) %>%
+  summarise() %>%
+  st_difference(water)
+
+##
+
+ggplot() +
+  geom_sf(data = background,
+          aes(), fill = '#7d7d7d', colour = NA) +
+  geom_sf(data = roads,
+          aes(), fill = NA, colour = '#ffffff', alpha = 0.5, size = 0.1) +
+  theme_bm_legend()
+
+##
+
+tic()
+
+difference <- st_difference(roads)
+intersection <- st_intersection(roads)
+
+toc()
+
+##
+
+nodes <-
+  intersection %>%
+  mutate(type = st_geometry_type(geometry)) %>%
+  filter(type == "POINT") %>%
+  select(-type) %>%
+  st_as_sf()
+
+##
+
+library(janitor)
+
+##
+
+join <- 
+  nodes %>% 
+  st_join(intersection) %>%
+  clean_names()
+
+##
+
+tic()
+
+verts <-
+  join %>%
+  drop_na() %>%
+  gather(variable, value, linearid_x, linearid_y) %>%
+  use_series(value) %>%
+  unique() %>%
+  as_tibble() %>%
+  rename(id = value)
+
+toc()
+
+##
+
+tic()
+
+verts <-
+  join %>%
+  drop_na() %>%
+  st_drop_geometry() %>%
+  gather(variable, value, linearid_x, linearid_y) %>%
+  distinct(value) %>%
+  rename(id = value)
+
+toc()
+
+##
+
+links <-
+  join %>%
+  drop_na() %>%
+  filter(linearid_x != linearid_y) %>%
+  select(linearid_x, linearid_y) %>%
+  rename(from = linearid_x,
+         to = linearid_y)
+
+##
+
+library(igraph)
+
+##
+
+graph <- graph_from_data_frame(links, vertices = verts, directed = FALSE)
+
+plot(graph,
+     vertex.size = 0.1,
+     vertex.label = '', 
+     alpha = 0.5)
