de50677ddfad5db22c2bf420f624c1a481ccf62f
andrew <arenn@design.upenn.edu>
2020-08-17 19:11:50 -0400

    New geofacet

--- R/geofacet.R
+++ R/geofacet.R
@@ -5,33 +5,47 @@ library(lubridate)
 library(sf)
 library(janitor)
 
-baseline <-
-  bind_rows(read_csv("data/indego/trips/indego-trips-2019-q1.csv.zip"),
-            read_csv("data/indego/trips/indego-trips-2018-q1.csv.zip")) %>%
+library(fs)
+
+files <- dir_ls("data/indego/trips")
+
+trips <-
+  bind_rows(read_csv("data/indego/trips/indego-trips-2020-q1.csv") %>%
+              mutate(start_time = mdy_hm(start_time),
+                     bike_id = as.character(bike_id),
+                     baseline = "0") %>%
+              select(-end_time),
+            read_csv("data/indego/trips/indego-trips-2020-q2.csv") %>%
+              mutate(start_time = mdy_hm(start_time),
+                     bike_id = as.character(bike_id),
+                     baseline = "0") %>%
+              select(-end_time),
+            read_csv("data/indego/trips/indego-trips-2019-q1.csv") %>%
+              mutate(bike_id = as.character(bike_id),
+                     baseline = "1") %>%
+              select(-end_time),
+            read_csv("data/indego/trips/indego-trips-2019-q2.csv") %>%
+              mutate(bike_id = as.character(bike_id),
+                     baseline = "1") %>%
+              select(-end_time)) %>%
   clean_names() %>%
   mutate(time = floor_date(start_time, "day"),
+         day = yday(time), 
          station_id = start_station) %>%
   filter(station_id != 3000) %>%
-  group_by(station_id, time) %>%
+  group_by(station_id, start_lat, start_lon, day, baseline) %>%
   summarise(n = n()) %>%
-  ungroup() %>%
-  mutate(day = yday(time)) %>%
-  group_by(station_id, day) %>%
-  summarise(baseline = mean(n, na.rm = TRUE))
+  ungroup()
 
 stations <- 
   read_csv("data/indego/indego-stations-2020-04-01.csv") %>%
-  clean_names()
-
-trips <- 
-  read_csv("data/indego/trips/indego-trips-2020-q1.csv.zip") %>%
   clean_names()
 
 points <-
   trips %>% 
   transmute(lat = start_lat,
             lon = start_lon, 
-            station = start_station) %>%
+            station = station_id) %>%
   distinct(station, .keep_all = TRUE) %>%
   drop_na(lon, lat) %>% 
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
@@ -41,12 +55,12 @@ points <-
 
 info <- 
   trips %>%
-  select(start_lat, start_lon, start_station) %>%
-  filter(start_station != 3000) %>%
-  distinct(start_station, .keep_all = TRUE) %>%
+  select(start_lat, start_lon, station_id) %>%
+  filter(station_id != 3000) %>%
+  distinct(station_id, .keep_all = TRUE) %>%
   rename(lat = start_lat,
          lon = start_lon,
-         station_id = start_station) %>%
+         station_id = station_id) %>%
   left_join(stations) %>%
   bind_cols(points) %>%
   st_as_sf(coords = c("lon", "lat"), remove = FALSE, crs = 4326) %>%
@@ -136,22 +150,6 @@ grid <-
 grid_preview(grid)
 grid_submit(grid)
 
-panel <-
-  trips %>%
-  mutate(start_time = str_replace_all(start_time, pattern = "/", replacement = "-"),
-         end_time = str_replace_all(end_time, pattern = "/", replacement = "-")) %>%
-  mutate(start_time = mdy_hm(start_time),
-         end_time = mdy_hm(end_time)) %>%
-  mutate(time = floor_date(start_time, "day"),
-         station_id = start_station) %>%
-  filter(station_id != 3000) %>%
-  group_by(station_id, time) %>%
-  summarise(n = n()) %>%
-  ungroup() %>%
-  mutate(day = yday(time)) %>%
-  select(day, station_id, n) %>%
-  left_join(baseline)
-
 crosswalk <-
   info %>%
   st_drop_geometry() %>%
@@ -170,7 +168,7 @@ theme_hor <- function () {
           axis.line.x = element_blank(),
           axis.ticks.y = element_line(size = 0.5, colour = 'black'),
           axis.ticks.x = element_blank(),
-          axis.text.x = element_blank(),
+        #  axis.text.x = element_blank(),
           axis.text.y = element_text(face = 'bold'),
           plot.title = element_text(face = 'bold', colour = 'grey50'),
           plot.subtitle =  element_text(face = 'plain', colour = 'black', size = 15),
@@ -182,19 +180,39 @@ theme_hor <- function () {
 pal <- read_csv("https://github.com/asrenninger/palettes/raw/master/turbo.txt", col_names = FALSE) %>% pull(X1)
 set <- sample(pal, 50, replace = TRUE)
 
-ggplot(panel %>%
+ggplot(trips %>%
          left_join(crosswalk) %>%
-         replace_na(list(baseline = 0)) %>%
+         replace_na(list(n = 0)) %>%
+         pivot_wider(names_from = baseline, values_from = n) %>% 
+         clean_names() %>%
+         rename(current = x0,
+                baseline = x1) %>%
          group_by(code, day) %>%
-         summarise(n = mean(n, na.rm = TRUE),
+         summarise(n = mean(current, na.rm = TRUE),
                    baseline = mean(baseline, na.rm = TRUE)) %>%
          mutate(decline = min(n) - mean(n)),
        aes(x = day, y = n - baseline, colour = decline)) +
   geom_line(show.legend = FALSE) +
-  scale_y_continuous(breaks = c(20, 0, -20)) +
+  #scale_y_continuous(breaks = c(20, 0, -20)) +
   scale_colour_gradientn(colours = rev(pal)) +
   facet_geo(~ code, grid = grid) +
   theme_hor() +
-  ggsave("grid.png", height = 8, width = 8, dpi = 300)
+  ggsave("grid.png", height = 12, width = 8, dpi = 300)
+
+trips %>%
+  group_by(day, baseline) %>%
+  summarise(sum = n()) %>%
+  ungroup() %>%
+  group_by(baseline) %>%
+  mutate(l1 = lag(sum),
+         l2 = lag(l1),
+         l3 = lag(l2),
+         l4 = lag(l3),
+         l5 = lag(l4),
+         l6 = lag(l5)) %>%
+  mutate(rolling = (sum + l1 + l2 + l3 + l4 + l5 + l6) / 7) %>%
+  ggplot(aes(day, rolling, colour = factor(baseline))) +
+  geom_line()
+
 
 
