f649d78f3c994a84722edaa31567b552d80a0691
andrew <arenn@design.upenn.edu>
2021-01-13 16:19:44 -0500

    New scripts

---
+++ R/resetup.R
@@ -0,0 +1,19 @@
+####################################
+## Resetting with new metro list
+####################################
+
+source("R/query.R")
+source("R/package.R")
+source("R/help.R")
+source("R/measure.R")
+
+## grab largest cities according to the census
+city_list <- 
+  read_csv("data/metrolist_two.csv") %>%
+  drop_na(metro_name) %>%
+  filter(rank > 0, rank < 101) %>%
+  pull(metro_name) %>%
+  unique()
+
+## looping through
+metrics <- map_df(city_list, ~get_metrics(.x, 1:12))
