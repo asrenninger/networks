8babf70646a8b2893f999accfc0fb3708ce62552
andrew <arenn@design.upenn.edu>
2022-06-22 11:29:57 +0100

    Debugging...

--- R/TfL.R
+++ R/TfL.R
@@ -24,7 +24,7 @@ status <-
   jsonlite::fromJSON() %>%
   glimpse() %>%
   mutate(envelope = purrr::map(envelope, ~jsonlite::fromJSON(.x)),
-       envelope = purrr::map(envelope, ~st_polygonize(st_linestring(.x)))) %>%
+         envelope = purrr::map(envelope, ~st_polygonize(st_linestring(.x)))) %>%
   rename(geometry = envelope) %>%
   st_as_sf(crs = 4326)
 
@@ -47,6 +47,19 @@ get_polygons <- function(roadworks){
   
 }
 
+get_polygons_2 <- function(roadworks){
+  
+  coordinates <- matrix(testing$geometry$coordinates[[1]][[1]], byrow = FALSE, ncol = 2)
+  lines <- 
+    st_linestring(coordinates) %>%
+    st_polygonize() %>%
+    st_sfc() %>% 
+    st_set_crs(4326)
+  
+  return(lines)
+  
+}
+
 modes <- "/Road/All/Disruption"
 dates <- "?startDate=2021-04-01&endDate=2021-04-30"
 types <- "&format=json"
@@ -59,7 +72,7 @@ disruptions <-
   jsonlite::fromJSON() %>% 
   glimpse() 
 
-url <- disruptions$url[1]
+url <- disruptions$url[!str_detect(disruptions$url, "PWC")][8]
 
 query <- paste0(base, url, dates, types)
 
@@ -70,8 +83,133 @@ testing <-
   jsonlite::fromJSON() %>% 
   glimpse() 
 
-poly <- get_polygons(testing)
+poly <- try(get_polygons(testing))
+
+if(pluck(class(poly), 1) %in% "try-error") {
+  
+  poly <- get_polygons_2(testing)
+  poly <- st_as_sf(poly)
+  
+}else{
+  
+  poly <- st_as_sf(poly)
+  
+}
 
 # see if it worked
 mapview::mapview(poly)
+
+# whole city
+all_poly <- 
+  map_dfr(disruptions$url[!str_detect(disruptions$url, "PWC")],
+          function(x){
+            
+            query <- paste0(base, x, dates, types)
+            print(query)
+            
+            roadworks <- 
+              httr::GET(query) %>%
+              magrittr::use_series("content") %>% 
+              rawToChar() %>%
+              jsonlite::fromJSON()
+            
+            
+            if(is.null(roadworks$geometry)) { 
+              
+              poly <- 
+                st_point(testing$geography$coordinates[[1]]) %>%
+                st_sfc() %>%
+                st_set_crs(4326) %>%
+                st_as_sf()
+              
+            }else{
+              poly <- try(get_polygons(roadworks))
+              
+              if(pluck(class(poly), 1) %in% "try-error") {
+                
+                poly <- get_polygons_2(roadworks)
+                poly <- st_as_sf(poly)
+                
+              }else{
+                
+                poly <- st_as_sf(poly)
+                
+              }
+              
+            }
+            
+            Sys.sleep(4)
+            return(poly)
+            
+          })
+
+# see if it worked... again
+all_poly %>% 
+  filter(st_geometry_type(x) != "POINT") %>% 
+  mapview::mapview()
+
+# different months? 
+modes <- "/Road/All/Disruption"
+dates <- "?startDate=2022-04-01&endDate=2022-04-30"
+types <- "&format=json"
+query <- paste0(base, modes, dates)
+
+disruptions <- 
+  httr::GET(query) %>%
+  magrittr::use_series("content") %>% 
+  rawToChar() %>%
+  jsonlite::fromJSON() %>% 
+  glimpse() 
+
+all_poly_2 <- 
+  map_dfr(disruptions$url[!str_detect(disruptions$url, "PWC")],
+          function(x){
+            
+            query <- paste0(base, x, dates, types)
+            print(query)
+            
+            roadworks <- 
+              httr::GET(query) %>%
+              magrittr::use_series("content") %>% 
+              rawToChar() %>%
+              jsonlite::fromJSON()
+            
+            
+            if(is.null(roadworks$geometry)) { 
+              
+              poly <- 
+                st_point(testing$geography$coordinates[[1]]) %>%
+                st_sfc() %>%
+                st_set_crs(4326) %>%
+                st_as_sf()
+              
+            }else{
+              poly <- try(get_polygons(roadworks))
+              
+              if(pluck(class(poly), 1) %in% "try-error") {
+                
+                poly <- get_polygons_2(roadworks)
+                poly <- st_as_sf(poly)
+                
+              }else{
+                
+                poly <- st_as_sf(poly)
+                
+              }
+              
+            }
+            
+            Sys.sleep(4)
+            return(poly)
+            
+          })
+
+# comparing to before... 
+all_poly_2 %>% 
+  filter(st_geometry_type(x) != "POINT") %>% 
+  mapview::mapview()
+
+
+
+
 
